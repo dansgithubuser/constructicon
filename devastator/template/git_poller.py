41e11f78fa861b2830221933c6f1eae7fbc68c3a
deisner <deisner@bionym.com>
2017-01-26 19:34:09 -0500

    better git poller

---
+++ devastator/template/git_poller.py
@@ -0,0 +1,127 @@
+import buildbot.util.state
+import buildbot.changes.base
+
+import os
+
+from twisted.internet import defer
+from twisted.python import log
+
+def _decode(x): return x.decode('utf-8')
+
+class ConstructiconGitPoller(buildbot.changes.base.PollingChangeSource, buildbot.util.state.StateMixin):
+	compare_attrs=['repo_url', 'work_dir', 'pollInterval', 'project']
+
+	def __init__(self, repo_url, work_dir='git-poller-work', poll_interval=600, project=''):
+		buildbot.changes.base.PollingChangeSource.__init__(self, name=repo_url, pollInterval=poll_interval)
+		self.repo_url=repo_url
+		self.work_dir=work_dir
+		self.project=project
+		self.branch_to_last_rev={}
+
+	def startService(self):
+		self.work_dir=os.path.join(self.master.basedir, self.work_dir)
+		d=self.getState('branch_to_last_rev', {})
+		def f(x): self.branch_to_last_rev=x
+		d.addCallback(f)
+		d.addCallback(lambda _: buildbot.changes.base.PollingChangeSource.startService(self))
+		d.addErrback(log.err, 'while initializing ConstructiconGitPoller repository')
+		return d
+
+	def describe(self):
+		return 'ConstructiconGitPoller repo_url: {} work_dir: {}'.format(self.repo_url, self.work_dir)
+
+	def _get_branches(self):
+		d=self._vc_cmd('ls-remote')
+		d.addCallback(lambda x: [i.split('\t')[1][11:] for i in x.splitlines() if '\trefs/heads/' in i])
+		return d
+
+	@defer.inlineCallbacks
+	def poll(self):
+		if not os.path.exists(os.path.join(self.work_dir, '.git')):
+			yield self._vc_cmd('clone', [self.repo_url, self.work_dir], '..')
+		yield self._vc_cmd('fetch')
+		branches=yield self._get_branches()
+		for i in branches:
+			rev=yield self._vc_cmd('rev-parse', ['origin/'+i])
+			yield self._process_changes(rev, i)
+			self.branch_to_last_rev[i]=rev
+		yield self.setState('branch_to_last_rev', self.branch_to_last_rev)
+
+	def _get_commit_comments(self, rev):
+		d=self._vc_cmd('log', ['--no-walk', '--format=%s%n%b', rev, '--'])
+		d.addCallback(_decode)
+		return d
+
+	def _get_commit_timestamp(self, rev):
+		d=self._vc_cmd('log', ['--no-walk', '--format=%ct', rev, '--'])
+		d.addCallback(lambda x: float(x))
+		return d
+
+	def _get_commit_files(self, rev):
+		d=self._vc_cmd('log', ['--name-only', '--no-walk', '--format=%n', rev, '--'])
+		def decode_file(file):
+			#git use octal char sequences in quotes when non ASCII
+			import re
+			match=re.match('^"(.*)"$', file)
+			if match: file=match.groups()[0].decode('string_escape')
+			return _decode(file)
+		def process(git_output):
+			import itertools
+			return [decode_file(file) for file in itertools.ifilter(lambda s: len(s), git_output.splitlines())]
+		d.addCallback(process)
+		return d
+
+	def _get_commit_author(self, rev):
+		d=self._vc_cmd('log', ['--no-walk', '--format=%aN <%aE>', rev, '--'])
+		d.addCallback(_decode)
+		return d
+
+	@defer.inlineCallbacks
+	def _process_changes(self, rev, branch):
+		if branch not in self.branch_to_last_rev: return
+		new_revs=yield self._vc_cmd('log', ['--format=%H', '{}..{}'.format(self.branch_to_last_rev[branch], rev), '--'])
+		new_revs=new_revs.split()
+		new_revs.reverse()
+		if len(new_revs): log.msg('git poller: processing {} changes: {} from {} branch {}'.format(len(new_revs), new_revs, self.repo_url, branch))
+		for i in new_revs:
+			dl=defer.DeferredList([
+				self._get_commit_timestamp(i),
+				self._get_commit_author(i),
+				self._get_commit_files(i),
+				self._get_commit_comments(i),
+			], consumeErrors=True)
+			results=yield dl
+			#check for failures
+			failures=[r[1] for r in results if not r[0]]
+			if failures:
+				#just fail on the first error; they're probably all related!
+				raise failures[0]
+			#
+			timestamp, author, files, comments=[r[1] for r in results]
+			from buildbot.util import epoch2datetime
+			yield self.master.addChange(
+				author=author,
+				revision=i,
+				files=files,
+				comments=comments,
+				when_timestamp=epoch2datetime(timestamp),
+				branch=branch,
+				project=self.project,
+				repository=self.repo_url,
+				src='git',
+			)
+
+	def _vc_cmd(self, command, args=[], path='.'):
+		path=os.path.normpath(os.path.join(self.work_dir, path))
+		from twisted.internet import utils
+		d=utils.getProcessOutputAndValue('git', [command]+args, path=path, env=os.environ)
+		#log.msg('ConstructiconGitPoller git {} in {}'.format(' '.join([command]+args), path))
+		def convert_nonzero_to_failure(results, command, args, path):
+			'utility to handle the result of getProcessOutputAndValue'
+			(stdout, stderr, code)=results
+			if code: raise EnvironmentError('command {} {} in {} on repo_url {} failed with exit code {}: {}'.format(
+				command, args, path, self.repo_url, code, stderr
+			))
+			return stdout.strip()
+		d.addCallback(convert_nonzero_to_failure, command, args, self.work_dir)
+		return d
---
+++ devastator/template/main.py
@@ -0,0 +1,393 @@
+import os; folder=os.path.realpath(os.path.dirname(__file__))
+import sys; sys.path.append(os.path.join(folder, '..', '..'))
+
+import common
+
+from buildbot.plugins import changes, buildslave, schedulers, steps, util
+from buildbot.status import html
+from buildbot.status.web import authz
+from buildbot.schedulers import forcesched
+from twisted.python import log
+
+import calendar, pprint
+
+ForceScheduler=schedulers.ForceScheduler
+Nightly=schedulers.Nightly
+AnyBranchScheduler=schedulers.AnyBranchScheduler
+
+cybertron=common.cybertron(os.path.join(folder, '..', '..'))
+
+parameter_prefix='parameter-'
+
+global_constructicons={{{constructicons}}}
+global_repo_urls={{{repo_urls}}}
+global_git_states={{{git_states}}}
+
+class ConfigException(Exception): pass
+
+class Config:
+	@staticmethod
+	def create(x):
+		if type(x)==dict: return Config({k: Config.create(v) for k, v in x.items()})
+		elif type(x)==list: return [Config.create(i) for i in x]
+		else: return x
+
+	def __init__(self, dict):
+		self.visited=set()
+		self.dict=dict
+
+	def __getitem__(self, k):
+		self.visited.add(k)
+		return self.dict[k]
+
+	def __contains__(self, k): return k in self.dict
+
+	def __repr__(self): return 'configuration'+str((self.visited, self.dict))
+
+	def update(self, dict): self.dict.update(dict)
+
+	def get(self, k, default):
+		if k in self.dict: return self[k]
+		else: return default
+
+	def items(self, mark_used=False):
+		if mark_used: self.visited=self.dict.keys()
+		return self.dict.items()
+
+	def keys(self): return self.dict.keys()
+
+	def values(self): return self.dict.values()
+
+	def unused(self, prefix=[]):
+		result=[prefix+[k] for k in self.dict if k not in self.visited]
+		def recurse_list(k, v, f):
+			if type(v)==list:
+				for i in range(len(v)): recurse_list(k+[i], v[i], f)
+			else: f(k, v)
+		def recurse(k, v):
+			if isinstance(v, Config): result.extend(v.unused(k))
+		for k, v in self.dict.items(): recurse_list(prefix+[k], v, recurse)
+		return result
+
+def repo_url_to_name(repo_url):
+	r=repo_url.split('/')[-1]
+	if r.endswith('.git'): r=r[:-4]
+	return r
+
+def factory(constructicon_name, builder_name, deps, commands, upload):
+	deps=sorted(deps)
+	def work_dir_renderer(*suffix):
+		@util.renderer
+		def work_dir(properties):
+			sep='/'
+			if all_slaves[properties['slavename']].get('platform', 0)=='windows': sep='\\'
+			return sep.join(('..', 'constructicons', constructicon_name, constructicon_name)+suffix)
+		return work_dir
+	result=util.BuildFactory()
+	def git_step(repo_url, work_dir):
+		return common.sane_step(steps.Git,
+			repourl=repo_url,
+			codebase=repo_url,
+			workdir=work_dir,
+			mode='full',
+			method='fresh',
+		)
+	def extract_parameters(dict):
+		return {i[len(parameter_prefix):]: str(j[0]) for i, j in dict.items() if i.startswith(parameter_prefix)}
+	@util.renderer
+	def env(properties): return extract_parameters(properties.asDict())
+	def format(command):
+		@util.renderer
+		def f(properties): return command.format(**extract_parameters(properties.asDict()))
+		return f
+	result.addSteps(
+		[
+			common.sane_step(steps.SetProperty,
+				name='devastator git state',
+				property='devastator_git_state',
+				value='{{{devastator_git_state}}}',
+			),
+			common.sane_step(steps.SetProperty,
+				name='constructicon git state',
+				property='git_state',
+				value=global_git_states[constructicon_name],
+			),
+			git_step(global_repo_urls[constructicon_name], work_dir_renderer()),
+		]
+		+
+		[git_step(i, work_dir_renderer('..', repo_url_to_name(i))) for i in deps]
+		+
+		[common.sane_step(steps.Compile,
+			name=commands[i][0],
+			command=format(commands[i][1]),
+			workdir=work_dir_renderer(),
+			env=env,
+		) for i in range(len(commands))]
+	)
+	for i, j in upload.items(True):
+		@util.renderer
+		def master_dest(properties, j=j):
+			return os.path.join(builder_name, str(properties['buildnumber'])+'-constructicon', j)
+		devastator_file_server_port=cybertron['devastator_file_server_port']
+		@util.renderer
+		def url(properties, j=j):
+			return (
+				'http://{{{devastator_host}}}:'+str(devastator_file_server_port)
+				+
+				'/'+builder_name+'/'+str(properties['buildnumber'])+'-constructicon'+'/'+j
+			)
+		step=steps.FileUpload(
+			slavesrc=i,
+			masterdest=master_dest,
+			url=url,
+			workdir=work_dir_renderer()
+		)
+		result.addStep(step)
+	return result
+
+def check_dict(x, key_type, value_type):
+	return (
+		(isinstance(x, Config) or type(x)!=dict)
+		and
+		all([type(i)==key_type and type(j)==value_type for i, j in x.items()])
+	)
+
+def check_list(x, item_type):
+	return (
+		type(x)==list
+		and
+		all([type(i)==item_type for i in x])
+	)
+
+def t_or_list_of(t, x): return type(x)==t or check_list(x, t)
+
+def intersect(dict1, dict2):
+	return set(dict1.keys())&set(dict2.keys())
+
+def get_base(key, default):
+	result=default
+	if 'builder_base' in cybertron:
+		if key in cybertron['builder_base']:
+			result=cybertron['builder_base'][key]
+	return Config.create(result)
+
+base={
+	'features': get_base('features', {}),
+	'deps': get_base('deps', []),
+	'precommands': get_base('precommands', []),
+	'commands': get_base('commands', []),
+	'upload': get_base('upload', {}),
+	'schedulers': get_base('schedulers', {}),
+}
+
+def check(spec, key, expectations):
+	for expectation in expectations:
+		if not expectation[0](spec, *expectation[1:-1]):
+			error(key+' '+expectation[-1]); return False
+		if not expectation[0](base[key], *expectation[1:-1]):
+			error('cybertron builder_base '+key+' '+expectation[-1]); return False
+	return True
+
+def number(list, prefix):
+	return [('{} {}'.format(prefix, i+1), v) for i, v in enumerate(list)]
+
+all_builders=[]
+all_schedulers=[]
+all_repo_urls=set(global_repo_urls.values())
+all_slaves=cybertron['slaves']
+errors=1
+slave_lock=util.SlaveLock('slave-lock', maxCount=1)
+for constructicon_name, constructicon_spec in global_constructicons.items():
+	constructicon_spec=Config.create(constructicon_spec)
+	git_state=global_git_states[constructicon_name]
+	def error(message):
+		try: name=builder_name
+		except NameError: name=constructicon_name
+		global errors
+		name+='-uniquifier-{}'.format(errors)
+		errors+=1
+		all_builders.append(util.BuilderConfig(
+			name=name,
+			description=git_state+' error: '+message,
+			slavenames=['none'],
+			factory=util.BuildFactory()
+		))
+		all_schedulers.append(ForceScheduler(
+			name=name+'-force',
+			builderNames=[name],
+		))
+	if not isinstance(constructicon_spec, Config):
+		error('constructicon is not a dict'); continue
+	#slaves
+	slaves=cybertron['slaves']
+	if 'slaves' in constructicon_spec:
+		prefix=constructicon_name+'-'
+		x=constructicon_spec['slaves']
+		for i, j in x.items():
+			if not i.startswith(prefix): break
+		else: i=None
+		if i!=None:
+			error("slave {} must start with {} but doesn't".format(i, prefix)); continue
+		slaves.update(constructicon_spec['slaves'])
+		all_slaves.update(slaves)
+	#builders
+	for builder_name, builder_spec in constructicon_spec['builders'].items(True):
+		#builder name
+		if type(builder_name)!=str:
+			error('builder name is not a str'); continue
+		builder_name=constructicon_name+'-'+builder_name
+		#builder spec
+		if not isinstance(builder_spec, Config):
+			error('builder spec is not a dict'); continue
+		#features
+		features=builder_spec.get('features', Config.create({}))
+		if not check(features, 'features', [[check_dict, str, str, 'is not a dict of str']]): continue
+		for k, v in features.items():
+			if k in base['features'] and v!=base['features'][k]: break
+		else: k=None
+		if k!=None:
+			error('feature {}: {} conflicts with cybertron builder_base {}'.format(k, v, base['features'][k])); continue
+		features.update(base['features'])
+		slave_names=[]
+		for slave_name, slave_features in slaves.items():
+			for feature, value in features.items(True):
+				if feature not in slave_features: break
+				if slave_features[feature]!=value: break
+			else: slave_names.append(slave_name)
+		if not len(slave_names):
+			error('no matching slaves'); continue
+		#deps
+		deps=builder_spec.get('deps', [])
+		if not check(deps, 'deps', [[check_list, str, 'is not a list of str']]): continue
+		deps+=[i for i in base['deps'] if i not in deps]
+		all_repo_urls.update(deps)
+		#precommands
+		precommands=builder_spec.get('precommands', [])
+		if not check(precommands, 'precommands', [
+			[lambda x: type(x)==list, 'is not a list'],
+			[lambda x: all([t_or_list_of(str, i) for i in x]), 'contains a precommand that is not a str or list of str'],
+		]): continue
+		precommands=number(base['precommands'], 'cybertron precommand')+number(precommands, 'precommand')
+		#commands
+		if 'commands' not in builder_spec:
+			error('no commands'); continue
+		commands=builder_spec['commands']
+		if not check(commands, 'commands', [
+			[lambda x: type(x)==list, 'is not a list'],
+			[lambda x: all([t_or_list_of(str, i) for i in x]), 'contains a command that is not a str or list of str'],
+		]): continue
+		commands=number(commands, 'command')+number(base['commands'], 'cybertron command')
+		#upload
+		upload=builder_spec.get('upload', Config.create({}))
+		if not check(upload, 'upload', [
+			[check_dict, str, str, 'is not a dict of str'],
+			[lambda x: all(['..' not in j for i, j in x.items()]), 'destination may not contain ..'],
+		]): continue
+		if set(base['upload'].values())&set(upload.values()):
+			error('upload conflicts with cybertron builder_base upload'); continue
+		upload.update(base['upload'])
+		#schedulers
+		schedulers=builder_spec.get('schedulers', Config.create({}))
+		if not check(schedulers, 'schedulers', [
+			[lambda x: isinstance(x, Config), 'is not a dict'],
+			[lambda x: all([type(i)==str for i in x.keys()]), "has a key that isn't a str"],
+			[lambda x: all([isinstance(j, Config) for i, j in x.items()]), "has a value that isn't a dict"],
+			[lambda x: all(['type' in j.keys() for i, j in x.items()]), 'contains a scheduler that is missing a type specification'],
+			[lambda x: all([j['type'] in ['force', 'time', 'commit'] for i, j in x.items()]), 'contains a scheduler that has an unknown type specification'],
+		]): continue
+		if intersect(schedulers, base['schedulers']):
+			error('schedulers conflicts with cybertron builder_base schedulers'); continue
+		schedulers.update(base['schedulers'])
+		builder_schedulers=[]
+		for name, spec in schedulers.items(True):
+			scheduler_args={}
+			#name
+			scheduler_args['name']=builder_name+'-'+name
+			#builderNames
+			scheduler_args['builderNames']=[builder_name]
+			#trigger
+			if spec['type']=='time':
+				scheduler_args['month']=spec.get('month', '*')
+				scheduler_args['dayOfMonth']=spec.get('day-of-month', '*')
+				scheduler_args['dayOfWeek']=spec.get('day-of-week', '*')
+				scheduler_args['hour']=spec.get('hour', 0)
+				scheduler_args['minute']=spec.get('minute', 0)
+				scheduler_args['branch']='master'
+			elif spec['type']=='commit':
+				scheduler_args['change_filter']=util.ChangeFilter(branch_re=spec.get('branch_regex', '.*'))
+			#codebases
+			x=[global_repo_urls[constructicon_name]]+deps
+			if spec['type']=='force':
+				scheduler_args['codebases']=[forcesched.CodebaseParameter(codebase=i) for i in x]
+			else:
+				scheduler_args['codebases']={i: {'repository': i} for i in x}
+			#parameters
+			parameters=spec.get('parameters', Config.create({}))
+			if spec['type']=='force':
+				scheduler_args['properties']=[util.StringParameter(name=parameter_prefix+i, default=j) for i, j in parameters.items(True)]
+			else:
+				scheduler_args['properties']={parameter_prefix+i: str(j) for i, j in parameters.items(True)}
+			#append
+			builder_schedulers.append({
+				'force': ForceScheduler,
+				'time': Nightly,
+				'commit': AnyBranchScheduler
+			}[spec['type']](**scheduler_args))
+		#append
+		f=factory(constructicon_name, builder_name, deps, precommands+commands, upload)
+		unused=builder_spec.unused()
+		if unused:
+			error('unused configuration keys\n'+pprint.pformat(unused)); continue
+		all_builders.append(util.BuilderConfig(
+			name=builder_name,
+			description=global_repo_urls[constructicon_name]+' '+git_state,
+			slavenames=slave_names,
+			factory=f,
+			locks=[slave_lock.access('exclusive')],
+		))
+		all_schedulers.extend(builder_schedulers)
+
+git_pollers=[ConstructiconGitPoller(
+	repo_url=i,
+	work_dir='git-poller-work-'+repo_url_to_name(i),
+	poll_interval=30,
+) for i in all_repo_urls]
+
+class DevastatorChangeSource(changes.MaildirSource):
+	name='DevastatorChangeSource'
+	def parse(self, m, prefix=None):
+		for i in git_pollers: i.poll()
+		return None
+
+change_sources=git_pollers
+if 'email_username' in cybertron:
+	maildir=os.path.join(folder, '..', '..', 'email', 'maildir')
+	change_sources.append(DevastatorChangeSource(maildir))
+
+BuildmasterConfig={
+	'db': {'db_url': 'sqlite:///state.sqlite'},
+	'slaves': [buildslave.BuildSlave(i, common.password) for i in all_slaves.keys()+['none']],
+	'protocols': {'pb': {'port': cybertron['devastator_slave_port']}},
+	'builders': all_builders,
+	'schedulers': all_schedulers,
+	'status': [html.WebStatus(cybertron['devastator_master_port'], authz=authz.Authz(
+		view=True,
+		forceBuild=True,
+		forceAllBuilds=True,
+		pingBuilder=True,
+		stopBuild=True,
+		stopAllBuilds=True,
+		cancelPendingBuild=True,
+		cancelAllPendingBuilds=True,
+		stopChange=True,
+		showUsersPage=True
+	))],
+	'codebaseGenerator': lambda chdict: chdict['repository'],
+	'change_source': change_sources,
+	'mergeRequests': False,
+	'debugPassword': 'sesame',
+	'changeHorizon': cybertron['horizon'],
+	'buildHorizon': cybertron['horizon'],
+	'eventHorizon': cybertron['horizon'],
+	'title': 'devastator {{{devastator_git_state}}}',
+}
