fd4bc168311297d5b2b27ec29307a119d11d1283
deisner <deisner@bionym.com>
2017-04-05 16:25:18 -0400

    allow testing ports to be selected by environment

--- cybertron.py
+++ cybertron.py
@@ -1,15 +1,19 @@
 #used in go.py example and tests
+import os
+
+offset=int(os.environ.get('CONSTRUCTICON_PORT_OFFSET', 0))
+
 cybertron={
 	'slaves': {
 		'slave-1': {'platform': 'linux', 'memory': 'goldfish'},
 		'slave-bad': {},
 	},
 	'megatron_hostname': 'localhost',
-	'megatron_master_port': 9120,
-	'megatron_slave_port': 9121,
-	'devastator_master_port': 9122,
-	'devastator_slave_port': 9123,
-	'devastator_file_server_port': 9124,
+	'megatron_master_port': 9120+offset,
+	'megatron_slave_port': 9121+offset,
+	'devastator_master_port': 9122+offset,
+	'devastator_slave_port': 9123+offset,
+	'devastator_file_server_port': 9124+offset,
 	'horizon': 100,
 	'builder_base': {
 		'accept': "features['memory']=='goldfish'",
