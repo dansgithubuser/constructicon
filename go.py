dansgithubuser/constructicon

commit ff3af939e681fabb1a80d27857f70900b9b532ea

simple build product management

go.py
@@ -11,6 +11,7 @@ stamp=''.join(random.choice(string.ascii_lowercase) for i in range(16))
 try: input=raw_input
 except: pass
 
+start=os.getcwd()
 folder=os.path.split(os.path.realpath(__file__))[0]
 os.chdir(folder)
 
@@ -328,6 +329,35 @@ def f(args):
 	forcer=Forcer(cybertron['megatron_hostname'], port, args.builder)
 	forcer.force(parameters=dict(zip(args.key, args.value)))
 
+def g(args):
+	constructicon=common.constructicon('.')
+	requirements=constructicon['builders'][args.builder]['get']
+	for builder, values in requirements.items():
+		#get builds from builder
+		forcer=Forcer(
+			cybertron['megatron_hostname'],
+			cybertron['devastator_master_port'],
+			builder,
+		)
+		builds=[i[1] for i in forcer.json_request('_all').items()]
+		#find build with max value
+		def calculate_value(build):
+			try: return [eval(value) for value in values]
+			except: return []
+		best_build=max(builds, key=lambda build: calculate_value(build))
+		assert calculate_value(best_build)!=[], 'no appropriate build!'
+		#download
+		downloads={}
+		for step in best_build['steps']: downloads.update(step.get('urls', {}))
+		for destination, url in downloads.items():
+			destination=os.path.join(start, '..', builder, destination)
+			folder=os.path.split(destination)[0]
+			if not os.path.exists(folder): os.makedirs(folder)
+			log('url', url)
+			with open(destination, 'w') as file:
+				print('{} --> {}'.format(url, destination))
+				file.write(retry(lambda: urlopen(url).read()))
+
 def example(args):
 	cybertron_store_folder(folder)
 	global cybertron
@@ -530,6 +560,11 @@ subparser.add_argument('--megatron', '-m', action='store_true', help='force on m
 subparser.add_argument('--builder', '-b', default='megatron-builder', help='which builder to force, default is megatron-builder')
 subparser.add_argument('--key'  , '-k', nargs='+', default=[], help='parameter keys, there should be an equal number of values')
 subparser.add_argument('--value', '-v', nargs='+', default=[], help='parameter values, there should be an equal number of keys')
+
+#-----get-----#
+subparser=subparsers.add_parser('g', help='get build results specified by builder in constructicon.py')
+subparser.set_defaults(func=g)
+subparser.add_argument('builder', help='builder to get build results for')
 
 #-----example-----#
 subparsers.add_parser('example', help='run example').set_defaults(func=example)

common.py
@@ -27,7 +27,9 @@ def execute(file_name, var):
 	with open(file_name) as file: contents=file.read()
 	x={
 		var: None,
-		'constructicon_slave_go': (lambda c: 'python '+'../'*6+'go.py '+c),
+		'constructicon_slave_go': lambda c: 'python {} {}'.format(
+			os.path.join(folder, 'go.py'), c
+		),
 	}
 	try: exec(contents, x)
 	except:

constructicon.py
@@ -4,6 +4,7 @@ constructicon={
 		'basic': {
 			'accept': "features['platform']=='linux'",
 			'commands': ['python go.py -h'],
+			'schedulers': ['force-7'],
 			'upload': {
 				'readme.md': 'readme-dest.md',
 				'constructicon.py': 'constructicon-dest.py',
@@ -28,11 +29,18 @@ constructicon={
 			'accept': "features['platform']=='snes' and features['memory']=='goldfish'",
 			'commands': ['python go.py -h'],
 		},
+		'get': {
+			'get': {'constructicon-basic': ["build['number']"]},
+			'commands': [
+				constructicon_slave_go('g get'),
+				'python ../constructicon-basic/constructicon-dest.py',
+			],
+		},
 	},
 	'schedulers': {
 		'force-7': {
 			'type': 'force',
-			'parameters': {'lucky_number': 7},
+			'parameters': {'lucky_number': 7, 'version': ''},
 		},
 		'time-42': {
 			'type': 'time',

devastator/template/main.py
@@ -349,6 +349,8 @@ for constructicon_name, constructicon_spec in global_constructicons.items():
 		]): continue
 		schedulers=set(schedulers+base['builder_base schedulers'])
 		for i in schedulers: scheduler_to_builders[full_scheduler_name(i)].append(builder_name)
+		#get - ignore
+		builder_spec.get('get', Config.create({})).items(True)
 		#append
 		f=factory(constructicon_name, builder_name, deps, precommands+commands, upload, zip, unzip, url)
 		unused=builder_spec.unused()

commit 73521afabdedbf71c2fdb3e83dacc800b7d9c4bc

python go.py c t returned 0
on mac

originally broken in ff3af939e681fabb1a8

go.py
@@ -217,6 +217,11 @@ class Forcer:
 	def json_request(self, build):
 		return self.json_request_generic('builders/{}/builds/{}'.format(self.builder, build))
 
+	def request(self, suffix):
+		url='{}/{}'.format(self.master, suffix)
+		log('url', url)
+		return retry(lambda: urlopen(url).read().decode('utf-8'))
+
 	def _request(self, url, data):
 		headers={'Referer': '{}/builders/{}'.format(self.master, self.builder)}
 		request=Request(url, urlencode(data).encode('utf-8'), headers)
@@ -609,7 +614,14 @@ def test(args):
 		basic_forcer.wait()
 		r=basic_forcer.json_request(-1)
 		#deps
-		expect(any(['crangen' in i['name'] for i in r['steps']]), 'builder_base dep', pprint.pformat(r))
+		logs_url=[i for i in r['steps'] if i['name']=='get'][0]['logs'][0][1]#this points to localhost:8080, not sure why
+		logs=basic_forcer.request(re.search('builders.*', logs_url).group(0))
+		got_repos=False
+		good=False
+		for line in logs.splitlines():
+			if line=='got repos': got_repos=True
+			if got_repos and 'crangen' in line: good=True; break
+		expect(good, 'builder_base dep', logs)
 		#precommands
 		expect(any([any(['cybertron precommand' in i['name']]) for i in r['steps']]), 'builder_base precommand', pprint.pformat(r))
 		#commands